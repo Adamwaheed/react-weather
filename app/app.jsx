8be3d09bf762b09c511c2ffc05b90f655cf3372d
Adam Waheed <i@live.mv>
2017-02-10 13:08:56 +0500

    Add State-0

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name : 'adam',
+  'location' : 'maldives'
+};
+
+var objTwo = {
+  age : 25,
+  ...objOne
+}
+
+console.log(objTwo);
+
+
 
 ReactDOM.render(
   <h1>Hello</h1>,
